5e2a5c3b0f009ce6afb177caf9f54d982dfc9e31
AnonymousOne-Repo <132542311+AnonymousOne-Repo@users.noreply.github.com>
2026-03-08 23:39:33 +0800

    Update input_builder.py

--- FieldCompletion/utils/input_builder.py
+++ FieldCompletion/utils/input_builder.py
@@ -8,3 +8,4 @@ def build_known_info(field_list, has_fields):
             known_info += f"And the information of {field} is ({has_fields[vuln_id].get(field, '')}). "
         prompt_input[vuln_id]['known_info'] = known_info.strip()
     return prompt_input
+    
